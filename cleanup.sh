67eeae2765e68c5ec4b110cf11f6b5388ec335c1
sousajo <sousajo@pop-os.localdomain>
2021-06-10 08:42:49 +0100

    add scripts to run and clean up this example

---
+++ cleanup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm *info
+rm *compiled
+rm *gcda
+rm *gcno
+rm main_test
+rm *binary
+rm -r *output
--- generate_report.sh
+++ generate_report.sh
@@ -1,14 +1,4 @@
-# Lcov examples
-
-### enum example - Goal
-
-Is an enum defined in a header file considered as instrumented in lcov calculation?
-
-### Run the example
-
-Ran using ``lcov 1.15``, `g++- 8.4.0`, and the `.lcovrc` commited here.
-
-````
+#!/bin/bash
 g++-8 -c foo.cpp -fprofile-arcs -ftest-coverage --std=c++14 -o foo.compiled
 g++-8 -c main.cpp -fprofile-arcs -ftest-coverage --std=c++14 -o main.compiled
 g++-8 -o foo_binary foo.compiled main.compiled -fprofile-arcs -ftest-coverage --std=c++14 
@@ -18,10 +8,4 @@ g++-8 -o main_test -fprofile-arcs -ftest-coverage --std=c++14 foo_test.cpp foo.c
 lcov --no-external --capture --directory . --output-file after_test.info
 lcov --add-tracefile baseline.info --add-tracefile after_test.info --output-file final_lcov.info
 genhtml --ignore-errors source final_lcov.info --legend --title "check if enum appears" --output-directory=genhtml_output
-
-````
-
-After executing this we can see that no `foo.h` is present in the html report.
-
-![report](./enum_report_screen.png)
 
